ebbc73ef05254719efbb67bc62bd51125e322f96
ohoon <0hoon5661@gmail.com>
2021-04-21 19:02:18 +0900

    Add post repository for jpaRepository

---
+++ src/main/java/com/github/springboard/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.github.springboard.repository;
+
+import com.github.springboard.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
