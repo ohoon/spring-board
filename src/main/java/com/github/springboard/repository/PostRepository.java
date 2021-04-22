49699cd2c7589a122cd4b49a209f92e979b9b66e
ohoon <0hoon5661@gmail.com>
2021-04-22 18:39:26 +0900

    Add findById method with member using entityGraph

--- src/main/java/com/github/springboard/repository/PostRepository.java
+++ src/main/java/com/github/springboard/repository/PostRepository.java
@@ -1,8 +1,14 @@
 package com.github.springboard.repository;
 
 import com.github.springboard.domain.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository {
+
+    @EntityGraph(attributePaths = {"member"})
+    Optional<Post> findWithMemberById(Long id);
 
 }
