3e3beb6f291a3877d102ea4e50e37a142be4f2fe
ohoon <0hoon5661@gmail.com>
2021-04-29 19:50:02 +0900

    Add init for roles

--- src/main/java/com/github/springboard/web/InitController.java
+++ src/main/java/com/github/springboard/web/InitController.java
@@ -1,8 +1,11 @@
 package com.github.springboard.web;
 
 import com.github.springboard.domain.Member;
+import com.github.springboard.domain.MemberRole;
+import com.github.springboard.domain.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,11 +30,37 @@ public class InitController {
 
         private final EntityManager em;
 
+        private final PasswordEncoder passwordEncoder;
+
         @Transactional
         public void init() {
-            Member memberA = Member.create("dudgns5661", "young6354", "영훈님", "0hoon5661@gmail.com");
-            Member memberB = Member.create("0hoon5661", "young6354", "Young.K", "dudgns5661@naver.com");
-            Member memberC = Member.create("ohoon", "young6354", "강영", "dudgns56611@naver.com");
+            Role roleA = Role.create("MEMBER");
+            Role roleB = Role.create("ADMIN");
+            em.persist(roleA);
+            em.persist(roleB);
+
+            Member memberA = Member.create(
+                    "dudgns5661",
+                    passwordEncoder.encode("young6354"),
+                    "영훈님",
+                    "0hoon5661@gmail.com",
+                    MemberRole.create(roleA)
+            );
+            Member memberB = Member.create(
+                    "0hoon5661",
+                    passwordEncoder.encode("young6354"),
+                    "Young.K",
+                    "dudgns5661@naver.com",
+                    MemberRole.create(roleA),
+                    MemberRole.create(roleB)
+            );
+            Member memberC = Member.create(
+                    "ohoon",
+                    passwordEncoder.encode("young6354"),
+                    "강영",
+                    "dudgns56611@naver.com",
+                    MemberRole.create(roleA)
+            );
             em.persist(memberA);
             em.persist(memberB);
             em.persist(memberC);
